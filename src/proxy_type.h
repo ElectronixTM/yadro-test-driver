154fd50ddb6dc566b8123b83055ac0af770f0d1b
ElectronixTM <etdiary1@gmail.com>
2025-05-05 19:23:50 +0300

    fix: split structure of proxy_t
    Now stat info is considered to be stored in stat_t and be bound to device for correct obtaining in _show functions

--- src/proxy_type.h
+++ src/proxy_type.h
@@ -17,6 +17,18 @@ struct sysfs_helper_t
 };
 
 /**
+ * Struct to store statistics of the proxy. Considered a private field of the
+ * device struct from sysfs_helper_t
+ */
+struct stat_t
+{
+  size_t read_rq_num; /**< total number of read requests on the device */
+  size_t write_rq_num; /**< total number of write requests on the device */
+  size_t total_read; /**< total bytes read via bios */
+  size_t total_write; /**< total bytes written to device */
+}
+
+/**
  * The struct, containing minimum required info to provide to sysfs, such
  * as amount of read and write requests and total sizes of all blocks, from
  * requests
@@ -25,10 +37,6 @@ struct proxy_t
 {
   struct dm_dev* dev; /**< device to forward all bios to */
   struct sysfs_helper_t sysfs;
-  size_t read_rq_num; /**< total number of read requests on the device */
-  size_t write_rq_num; /**< total number of write requests on the device */
-  size_t total_read; /**< total bytes read via bios */
-  size_t total_write; /**< total bytes written to device */
 };
 
 #endif
